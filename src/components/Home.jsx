a37ff246323635cf5673334717f0d39b5ca1d9c5
Tinu George <tinu.george@outlook.com>
2018-12-05 19:35:29 +0530

    add basic routing

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Home extends React.Component{
+	render() {
+		return (
+			<h2>I'm on the Home Page</h2>
+		);
+	}
+}
+
+export default Home;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import notFound from './404';
+
 
 const Router = () => (
-
-	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={App}></Route>
-		</Switch>
-	</BrowserRouter>
-
-	);
+  <BrowserRouter>
+    <div>
+    	<Header />
+	  	<Switch>
+    		<Route exact path="/" component={Home} />
+    		<Route exact path="/about" component={About} />
+    		<Route component={notFound} />
+    	</Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default Router;
